b3e83f6dedb757d6e266bf9d2f14a472140de487
Christopher Waas <43119886+chriswaas@users.noreply.github.com>
2018-09-21 13:20:50 -0400

    Initial commit

---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Example2018
+@show Polynomial([1,2,3])
+@show evaluate(Polynomial([1,2,3]),2)
+@show Polynomial([1,2,3]) + Polynomial([4,5])
+@show 3*Polynomial([1,2,4,42])
+@show deriv(Polynomial([1,2,3]))
